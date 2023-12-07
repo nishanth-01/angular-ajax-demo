4059410ac8fcbfc4305bff06db283c5b2e4b68f9
Nishanth M <nishanth_m@outlook.com>
2023-12-07 12:17:04 +0530

    refactor: app, common

--- src/app/common.ts
+++ src/app/common.ts
@@ -20,12 +20,3 @@ export function validCredentials(id: string, pwd: string): boolean {
   return pattern.userId.test(id) && pattern.password.test(id);
 }
 
-export function login(cred?: UserCredentials): User | null {
-  if (cred === undefined) {
-    // check login status return user info if logged in
-    return {id: '1234', role: 'admin'};
-  }
-  // try to login
-
-  return {id: cred.id, role: 'general'};
-}
