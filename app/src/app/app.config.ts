5ee4e5a2453e4116c7e2375fe9ae001dfa66b896
Nishanth M <nishanth_m@outlook.com>
2023-12-15 16:45:05 +0530

    refactor(app): hash location strategy instead of push state

--- app/src/app/app.config.ts
+++ app/src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig, importProvidersFrom  } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     importProvidersFrom(HttpClientModule)
   ]
 };
